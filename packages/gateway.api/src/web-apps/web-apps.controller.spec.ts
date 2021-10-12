ac2dca5b497df0c2550930f4ae589416392e0041
Anthony Bonta <zthun.coding@gmail.com>
2021-10-12 16:58:07 -0500

    feat: added the web-apps route

---
+++ packages/gateway.api/src/web-apps/web-apps.controller.spec.ts
@@ -0,0 +1,35 @@
+/**
+ * @jest-environment node
+ */
+/* eslint-disable require-jsdoc */
+import { IZWebApp, ZWebAppBuilder } from '@zthun/works.core';
+import { createMocked } from '@zthun/works.jest';
+import { ZAppsService } from '../apps/apps.service';
+import { ZWebAppsController } from './web-apps.controller';
+
+describe('ZWebAppsController', () => {
+  let apps: IZWebApp[];
+  let service: jest.Mocked<ZAppsService>;
+
+  function createTestTarget() {
+    return new ZWebAppsController(service);
+  }
+
+  beforeEach(() => {
+    apps = [new ZWebAppBuilder().id('one').name('One').build(), new ZWebAppBuilder().id('two').name('Two').build()];
+
+    service = createMocked<ZAppsService>(['listWebApps']);
+    service.listWebApps.mockResolvedValue(apps);
+  });
+
+  describe('List', () => {
+    it('should return all apps.', async () => {
+      // Arrange
+      const target = createTestTarget();
+      // Act
+      const actual = await target.list();
+      // Assert
+      expect(actual).toEqual(apps);
+    });
+  });
+});
